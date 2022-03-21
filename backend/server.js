12e463d358d43132794fbf614562f3f736dd5619
Mohamed Gad <mohamedgad149@gmail.com>
2022-03-21 19:18:58 +0200

    retrieved data from the API

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,16 @@
+require('dotenv').config();
 const express = require('express');
 const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 
 const app = express();
 
 const API_URL = `https://api.worldweatheronline.com/premium/v1/weather.ashx`;
+const key = process.env.KEY;
 app.get('/', async ( req, res ) => {
     const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
-    let data = await fetch(API_URL + '?q=' + ip );
-    // data = await data.json();
+    console.log({ip})
+    let data = await fetch(API_URL + `?key=${key}&q=${ip}&format=json`);
+    data = await data.json();
     console.log(data);
     res.send(data);
 });
