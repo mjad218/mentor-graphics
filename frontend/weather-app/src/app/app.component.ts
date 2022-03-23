742e1736594be7fa98239155cc14c62760023c35
Mohamed Gad <mohamedgad149@gmail.com>
2022-03-23 17:54:59 +0200

    back end ready

--- frontend/weather-app/src/app/app.component.ts
+++ frontend/weather-app/src/app/app.component.ts
@@ -7,7 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'weather app';
+  KEY = `AIzaSyBi808bo93OVTLmyGzBka0GaQFjJiTHCMc`;
+
+  getPosition(latitude: number, longitude: number) {
+    this.MAPS_API = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&key=${this.KEY}`;
+    console.log(this.MAPS_API);
+  }
   ngOnInit () : void {
-    navigator.geolocation.getCurrentPosition( (position) => {console.log(position)}, (err) => {console.log(err)});
+    navigator.geolocation.getCurrentPosition( (position) => {this.getPosition(position.coords.latitude , position.coords.longitude)}, (err) => {console.log(err)});
   }
 }
